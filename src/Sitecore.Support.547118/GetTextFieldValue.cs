c0b0c99a4a122846a0c628150d52ce9e2b4371ef
SC-IDO <ido@sitecore.net>
2022-08-12 17:32:52 +0300

    Initial commit with predefined code #547118

---
+++ src/Sitecore.Support.547118/GetTextFieldValue.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.RenderField;
+using System;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.RenderField
+{
+    public class GetTextFieldValue
+    {
+        public void Process([NotNull] RenderFieldArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            this.EncodeFieldValue(args);
+
+            var typeKey = args.FieldTypeKey;
+            if (typeKey.Equals("text", StringComparison.InvariantCulture) || typeKey.Equals("single-line text", StringComparison.InvariantCulture))
+            {
+                args.WebEditParameters.Add("prevent-line-break", "true");
+            }
+        }
+
+        protected virtual void EncodeFieldValue([NotNull] RenderFieldArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (Settings.Rendering.HtmlEncodedFieldTypes.Contains(args.FieldTypeKey))
+            {
+                args.Result.FirstPart = HttpUtility.HtmlEncode(args.Result.FirstPart);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.547118/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.547118")]
+[assembly: AssemblyProduct("Sitecore.Support.547118")]
+[assembly: ComVisible(false)]
